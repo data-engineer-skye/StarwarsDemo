9bfd7150f6bf99ef42eb3383cd380fe639cc52f0
data-engineer-skye <skye.learning.palace@gmail.com>
2023-09-05 23:49:38 +0800

    refactor: numeric compare function

--- Util/compare.py
+++ Util/compare.py
@@ -1,11 +1,38 @@
-def is_numeric_value_met_condition(value, compare_type, require_value):
-    compare_dict = {
-        "over": ">",
-        "less": "<",
-        "equal": "==",
-        "greater or equal": ">=",
-        "less or equal": "<="
-    }    
+import allure
+import logging
+from typing import Union
 
-    result = eval(f"{value} {compare_dict[compare_type]} {require_value}")
-    return result
+def is_numeric_value_met_condition(value: str, compare_type: str, require_value: Union[int, float]) -> bool:
+    """
+    Check if a numeric value meets a specified comparison condition.
+
+    Parameters:
+    - value: The numeric value to be compared.
+    - compare_type: The type of comparison to perform (e.g., 'equal', 'greater', 'less').
+    - require_value: The numeric value to compare against.
+
+    Returns:
+    - bool: True if the condition is met, False otherwise.
+
+    Raises:
+    - ValueError: If an invalid comparison type is provided.
+    """
+
+    # Define comparion function
+    comparison_functions = {
+        "equal": lambda x, y: x == y,
+        "greater": lambda x, y: x > y,
+        "less": lambda x, y: x < y,
+        "greater or equal": lambda x, y: x >= y,
+        "less or equal": lambda x, y: x <= y,
+        "greater or over": lambda x, y: x >= y,
+    }
+
+    if compare_type not in comparison_functions:
+        raise ValueError(f"Invalid comparison type: {compare_type}")
+
+    return comparison_functions[compare_type](float(value), require_value)
+
+@allure.step("Check assert")
+def assert_check(actual_value, expect_value):
+    assert actual_value ==  expect_value, f"Assert Fail, Expect: {expect_value}, but Actual: {actual_value}"  
